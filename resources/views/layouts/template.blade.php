5331dc49f34225a06cf60249cb9d88d3354d1a1e
Seantheman01 <sean.v.g@outlook.com>
2025-01-15 11:33:45 +0100

    better navbar

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,18 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>@yield('title')</title>
     <!-- @vite('resources/css/app.css') -->
     @stack('styles')
 </head>
 <body>
-    <nav>
+    <!-- <nav>
         <ul>
+            <div class="navbar-brand" href="#">
+                <div class="title">Jukebox</div>
+            </div>
             <li><a href="/">Home</a></li>
-            <li><a href="{{route('song.index')}}">Songs</a></li>
-            <li><a href="{{route('genre.index')}}">Genres</a></li>
+            <div class="dropdown">Songs
+                <div class="dropdown-content">
+                    <a href="{{route('song.create')}}">Create</a>
+                    <a href="{{route('song.index')}}">All</a>
+                </div>
+            </div>
         </ul>
-    </nav>
+    </nav> -->
+
+    <div class="menu">
+        <a href="/">Home</a>
+    </div>
 
     @yield('content')
 
