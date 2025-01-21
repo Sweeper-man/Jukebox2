a75b6c55b46a0d123dbeb98cdf217c8fbff0afa2
Seantheman01 <sean.v.g@outlook.com>
2025-01-21 11:18:51 +0100

    cards fixed

--- resources/views/songs/create.blade.php
+++ resources/views/songs/create.blade.php
@@ -1,29 +1,33 @@
 @extends('layouts.template')
 
 @section("content")
-    <form action="/songs/store" method="POST">
-        @csrf
-        <label for="name">Vul hier de naam van het liedje in:</label>
-        <input id="name" name="name" type="text">
+    <div class="section">
+        <div class="card">
+            <form action="/songs/store" method="POST">
+                @csrf
+                <label for="name">Vul hier de naam van het liedje in:</label>
+                <input id="name" name="name" type="text">
 
-        <label for="">Vul hier de duration van het liedje in:</label>
-        <input name="duration" type="number">
+                <label for="">Vul hier de duration van het liedje in:</label>
+                <input name="duration" type="number">
 
-        <label for="">Vul hier het genre id in:</label>
-        <input name="genre_id" type="text">
+                <label for="">Vul hier het genre id in:</label>
+                <input name="genre_id" type="text">
 
-        <input type="submit" value="Maak nummer aan">
+                <input type="submit" value="Maak nummer aan">
 
-        @error("name")
-            <p style="color:red">{{$message}}</p>
-        @enderror
-        
-        @error("duration")
-            <p style="color:red">{{$message}}</p>
-        @enderror
+                @error("name")
+                    <p style="color:red">{{$message}}</p>
+                @enderror
+                
+                @error("duration")
+                    <p style="color:red">{{$message}}</p>
+                @enderror
 
-        @error("genre_id")
-            <p style="color:red">{{$message}}</p>
-        @enderror
-    </form>
+                @error("genre_id")
+                    <p style="color:red">{{$message}}</p>
+                @enderror
+            </form>
+        </div>
+    </div>
 @endsection
--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -6,17 +6,13 @@
 @endsection
 
 @section('content')
-    <h1>Hier komen de songs te staan</h1>
-    <ul>
-        @foreach($songs as $song)
-            <li>{{$song->name}} - {{$song->duration}} - {{$song->genre_id}}</li>
-        @endforeach
-    </ul>
-    
     <div class="section">
         <div class="card">
-            <img src="https://via.placeholder.com/300" alt="image">
-            <p>Songs</p>
+            <ul>
+                @foreach($songs as $song)
+                    <li>{{$song->name}} - {{$song->duration}} - {{$song->genre_id}}</li>
+                @endforeach
+            </ul>
         </div>
     </div>
 @endsection
