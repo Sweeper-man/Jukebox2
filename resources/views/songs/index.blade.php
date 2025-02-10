1fbc463312065b3659b39c671cc006e24039134b
Seantheman01 <sean.v.g@outlook.com>
2025-02-10 11:01:35 +0100

    card disappears when song is deleted

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -6,16 +6,18 @@
 
 @section('content')
     <div class="section">
-        <div class="card">
-            @foreach($songs as $song)
-                <img src="https://placehold.co/100" alt="image">
-                {{$song->name}} - {{$song->duration}} - {{$song->genre_id}}
-                 <form action="{{ route('song.delete', $song->id) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                        <button type="submit" class="button">Delete</button>
-                 </form>
-            @endforeach
-        </div>
+        @if($songs->count())
+            <div class="card">
+                @foreach($songs as $song)
+                    <img src="https://placehold.co/100" alt="image">
+                    {{$song->name}} - {{$song->duration}} - {{$song->genre_id}}
+                    <form action="{{ route('song.delete', $song->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                            <button type="submit" class="button">Delete</button>
+                    </form>
+                @endforeach
+            </div>
+        @endif
     </div>
 @endsection
