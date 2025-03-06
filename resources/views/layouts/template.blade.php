3a5bbe1f19eac614c44ebd468492a4804bdbfaab
Seantheman01 <sean.v.g@outlook.com>
2025-03-06 09:37:46 +0100

    name changes

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -12,14 +12,14 @@
 <body>
     <div class="menu">
         <a href="/">Home</a>
-        <div class="dropdown">Songs
-            <div class="dropdown-content">
+        <div class="menuDropdown">Songs
+            <div class="menuDropdown-content">
                 <a href="{{route('song.create')}}">Create</a>
                 <a href="{{route('song.index')}}">All</a>
             </div>
         </div>
-        <div class="dropdown">Genres
-            <div class="dropdown-content">
+        <div class="menuDropdown">Genres
+            <div class="menuDropdown-content">
                 <a href="{{route('genre.create')}}">Create</a>
                 <a href="{{route('genre.index')}}">All</a>
             </div>
