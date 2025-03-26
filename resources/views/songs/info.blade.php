f55a46d2f334df698fc675f1da02f83d81e99a6d
Seantheman01 <sean.v.g@outlook.com>
2025-03-26 09:18:01 +0100

    .

--- resources/views/songs/info.blade.php
+++ resources/views/songs/info.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     @foreach($songs as $song)
         <h1>{{ $song->name }}</h1>
-        <p><strong>Duration:</strong> {{ $song->duration }} seconds</p>
-        <p><strong>Genre ID:</strong> {{ $song->genre_id }}</p>
+        <p><strong>Duration: </strong>{{ $song->duration }} seconds</p>
+        <p><strong>Genre ID: </strong>{{ $song->genre_id }}</p>
     @endforeach
 @endsection
